ce59bfbc08941c7ef1867ffa7215fed4500c4593
Azmodessu <golovrkd@yandex.ru>
2024-02-23 13:37:31 +0300

    clear for Winter_shoes

--- Winter_shoes.py
+++ Winter_shoes.py
@@ -50,28 +50,35 @@ for post in posts:
 wb.save('wintershoes.xlsx')
 wb.close()
 
+def export():
+    con = sq.connect("sneakers.db")
+    cur = con.cursor()
 
-con = sq.connect("sneakers.db")
-cur = con.cursor()
+    cur.execute("""CREATE TABLE IF NOT EXISTS winter_shoes (
+    title TEXT,
+    price TEXT,
+    image TEXT
+    )""")
 
-cur.execute("""CREATE TABLE IF NOT EXISTS winter_shoes (
-title TEXT,
-price TEXT,
-image TEXT
-)""")
+    import openpyxl
 
-import openpyxl
+    book = openpyxl.open("wintershoes.xlsx", read_only=True)
 
-book = openpyxl.open("wintershoes.xlsx", read_only=True)
+    sheet = book.active
 
-sheet = book.active
-
-for row in range(2, sheet.max_row + 1):
-    data = []
-    for col in range(1, 4):
-        value = sheet.cell(row, col).value
+    for row in range(2, sheet.max_row + 1):
+        data = []
+        for col in range(1, 4):
+            value = sheet.cell(row, col).value
         data.append(value)
-    cur.execute("INSERT INTO winter_shoes VALUES (?, ?, ?);", (data[0], data[1], data[2]))
+        cur.execute("INSERT INTO winter_shoes VALUES (?, ?, ?);", (data[0], data[1], data[2]))
 
-con.commit()
-con.close()
+    con.commit()
+    con.close()
+
+def clear():
+    con = sq.connect("sneakers.db")
+    cur = con.cursor()
+    cur.execute("DELETE FROM sneakers")
+    con.commit()
+    con.close()
